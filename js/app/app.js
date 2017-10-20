5a6ee19853006fe699f3a886a377e381d186ab40
AkosuaA <nattuah@gmail.com>
2017-10-20 21:32:11 +0000

    almost done

--- js/app/app.js
+++ js/app/app.js
@@ -1,34 +1,67 @@
-// Since we will be making use of 3rd party functions (eg. navigator.geolocation.getCurrentPosition) which are not native javascript functions we will need
-// to add this function to the list of native javascript functions to allow javascript identify and execute it each time its called.
-// This is done by using the addEventListener() function.
-//
-document.addEventListener("deviceready", onDeviceReady, false);
+    // Since we will be making use of 3rd party functions (eg. navigator.geolocation.getCurrentPosition) which are not native javascript functions we will need
+    // to add this function to the list of native javascript functions to allow javascript identify and execute it each time its called.
+    // This is done by using the addEventListener() function.
 
+    //define global variables
+    var latitude = undefined;
+    var longitude = undefined;
+    var map;
+    var marker;
 
-//We decide to create a function to handle the 3rd party functions (eg. navigator.geolocation.getCurrentPosition)
-// which we earlier added to the native functions of the javascript
-function onDeviceReady() {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}
+    //We decide to create a function to handle the 3rd party functions (eg. navigator.geolocation.getCurrentPosition)
+    // which we earlier added to the native functions of the javascript
 
-// onSuccess Geolocation
-//
-function onSuccess(position) {
+    function getLocation(){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, {enableHighAccuracy: true});
+    }
 
-    var element = document.getElementById('geolocation');
-    element.innerHTML = 'Latitude: ' + position.coords.latitude  + '<br />' +
-        'Longitude: '          + position.coords.longitude             + '<br />' +
-        'Altitude: '           + position.coords.altitude              + '<br />' +
-        'Accuracy: '           + position.coords.accuracy              + '<br />' +
-        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-        'Heading: '            + position.coords.heading               + '<br />' +
-        'Speed: '              + position.coords.speed                 + '<br />' +
-        'Timestamp: '          + position.timestamp          + '<br />';
-}
+    // onSuccess Geolocation
+    function onSuccess(position) {
 
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-        'message: ' + error.message + '\n');
-}
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        insertMarker();
+
+    }
+
+    // onError Callback receives a PositionError object
+    function onError(error) {
+        alert('Something went wrong'    + '\n' +
+            'Kindly make sure your GPS is on' + '\n');
+    }
+
+    //function to initialize map
+    function initMap(){
+        var options = {
+            center: {lat: 5.5557, lng: -0.1963},
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById('map'), options); 
+        marker = new google.maps.Marker({
+            position: {lat:5.5557, lng: -0.1963},
+            map: map
+        });
+
+        marker.setVisible(false);
+
+    }
+
+    //function to insert location on map
+    function insertMarker(){
+        var position = new google.maps.LatLng(latitude,longitude);
+        map.setCenter(position);
+        marker.setPosition(position);
+        marker.setVisible(true);
+    }
+
+    //function to make marker visible
+    function makeVisible() {
+        marker.setVisible(true);
+    }
+
+    //function to hide marker 
+    function hide() {
+        marker.setVisible(false);
+    }
